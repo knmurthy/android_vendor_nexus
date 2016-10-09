9f4cb36651487e85e105f861fc2c0090caa7dfc8
knmurthy <knmurthy@outlook.com>
2016-10-09 12:14:24 +0530

    Remove unneeded products and configs

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -13,33 +13,9 @@
 # limitations under the License.
 
 # Add Stock Lollipop bootanimation based on device
-ifneq ($(filter nexus_angler,$(TARGET_PRODUCT)),)
-    PRODUCT_COPY_FILES += \
-        vendor/nexus/prebuilt/bootanimation/angler.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter nexus_bullhead,$(TARGET_PRODUCT)),)
-    PRODUCT_COPY_FILES += \
-        vendor/nexus/prebuilt/bootanimation/bullhead.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter nexus_deb,$(TARGET_PRODUCT)),)
-    PRODUCT_COPY_FILES += \
-        vendor/nexus/prebuilt/bootanimation/flo.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter nexus_flo,$(TARGET_PRODUCT)),)
-    PRODUCT_COPY_FILES += \
-        vendor/nexus/prebuilt/bootanimation/flo.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter nexus_flounder,$(TARGET_PRODUCT)),)
-    PRODUCT_COPY_FILES += \
-        vendor/nexus/prebuilt/bootanimation/flounder.zip:system/media/bootanimation.zip
-endif
 ifneq ($(filter nexus_hammerhead,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/nexus/prebuilt/bootanimation/hammerhead.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter nexus_shamu,$(TARGET_PRODUCT)),)
-    PRODUCT_COPY_FILES += \
-        vendor/nexus/prebuilt/bootanimation/shamu.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter nexus_mako,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
